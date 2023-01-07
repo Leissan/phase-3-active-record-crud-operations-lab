3a348265cdda47782b376f7d582340fc96038a7b
Leissan Khan <leissan.khan@gmail.com>
2023-01-07 15:49:41 -0500

    commit tree 66497d3ae61ee6480d617aa58bc6564f1b8f515c

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,50 @@
 class Movie < ActiveRecord::Base
 
+    def self.create_with_title(title)
+        #take in a title , instatiate a movie w a title and save it to databse
+        Movie.create(title: title)
+    end
+
+    def self.first_movie
+        Movie.first
+    end
+
+    def self.last_movie
+        Movie.last
+    end
+
+    def self.movie_count
+        Movie.count
+    end
+
+    def self.find_movie_with_id(id)
+        Movie.find(id)
+    end
+
+    def self.find_movie_with_attributes(attributes)
+        Movie.find_by(attributes)
+    end
+
+    def self.find_movies_after_2002
+        Movie.where('release_date > 2002')
+    end
+
+    def update_with_attributes(attr)
+        self.update(attr)
+    end
+
+    def self.update_all_titles(title)
+        self.update(title: title)
+    end
+
+    def self.delete_by_id(id)
+        self.delete(id)
+    end
+
+    def self.delete_all_movies
+        self.destroy_all
+    end
+
+ 
+
 end
